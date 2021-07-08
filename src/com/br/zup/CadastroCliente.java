abf2166adb0b9f61bdefe54ec9a68ca62569aaba
Kaio <kaio.moraes@zup.com.br>
2021-07-08 16:45:17 -0300

    Criar o encapsulamento  das classes Cadastro do funcionario e cliente

--- src/com/br/zup/CadastroCliente.java
+++ src/com/br/zup/CadastroCliente.java
@@ -6,16 +6,39 @@ public class CadastroCliente {
     protected String enderenco;
     protected String cpfOuRg;
     protected String contaAssociada;
+
+    public CadastroCliente() {
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getEnderenco() {
+        return enderenco;
+    }
+
+    public void setEnderenco(String enderenco) {
+        this.enderenco = enderenco;
+    }
+
+    public String getCpfOuRg() {
+        return cpfOuRg;
+    }
+
+    public void setCpfOuRg(String cpfOuRg) {
+        this.cpfOuRg = cpfOuRg;
+    }
+
+    public String getContaAssociada() {
+        return contaAssociada;
+    }
+
+    public void setContaAssociada(String contaAssociada) {
+        this.contaAssociada = contaAssociada;
+    }
 }
-//Os Clientes devem ter as seguintes informações:
-//- Nome
-//- Endereço
-//- Documentos pessoais
-//- Uma conta associada
-//Os Funcionários devem ter as seguintes informações:
-//- Nome
-//- Endereço
-//- Documentos pessoais
-//- Cargo na empresa
-//- Salario
-//- Uma conta associada
--- src/com/br/zup/CadastroFuncionario.java
+++ src/com/br/zup/CadastroFuncionario.java
@@ -5,4 +5,22 @@ public class CadastroFuncionario extends CadastroCliente {
     protected String cargoNaEmpresa;
     protected double salario;
 
+    public CadastroFuncionario() {
+    }
+
+    public String getCargoNaEmpresa() {
+        return cargoNaEmpresa;
+    }
+
+    public void setCargoNaEmpresa(String cargoNaEmpresa) {
+        this.cargoNaEmpresa = cargoNaEmpresa;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
