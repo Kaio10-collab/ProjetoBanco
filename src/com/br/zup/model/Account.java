c1caa57ae9184827a2c7417dad5fb0bc6fd918e0
Kaio <kaio.moraes@zup.com.br>
2021-08-20 16:53:50 -0300

    gerando constructor, get e set da classe Account

--- src/com/br/zup/model/Account.java
+++ src/com/br/zup/model/Account.java
@@ -2,61 +2,30 @@ package com.br.zup.model;
 
 public class Account {
 
-    public double salario;
-    public String cargo;
-    public String nome;
-    public String cpf;
-    public int senha;
-    public String endereco;
+    protected String numberAccount;
+    protected Double balance;
 
     public Account() {
     }
 
-    public double getSalario() {
-        return salario;
+    public Account(String numberAccount, Double balance) {
+        this.numberAccount = numberAccount;
+        this.balance = balance;
     }
 
-    public void setSalario(double salario) {
-        this.salario = salario;
+    public String getNumberAccount() {
+        return numberAccount;
     }
 
-    public String getCargo() {
-        return cargo;
+    public void setNumberAccount(String numberAccount) {
+        this.numberAccount = numberAccount;
     }
 
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
+    public Double getBalance() {
+        return balance;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public int getSenha() {
-        return senha;
-    }
-
-    public void setSenha(int senha) {
-        this.senha = senha;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
+    public void setBalance(Double balance) {
+        this.balance = balance;
     }
 }
